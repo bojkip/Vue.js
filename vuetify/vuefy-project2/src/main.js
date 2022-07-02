33499f7ad25bce4048e40332d22df3c2bff4c0dd
bojkip <bojan.petkovic@protonmail.com>
2022-07-03 00:35:53 +0200

    1.4

--- vuetify/vuefy-project2/src/main.js
+++ vuetify/vuefy-project2/src/main.js
@@ -1,28 +1,14 @@
 import Vue from 'vue';
-import Router from 'vue-router';
 import App from './App.vue';
-import TheHome from './views/TheHome.vue';
 import vuetify from './plugins/vuetify';
+import router from './router';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false;
 
-Vue.use(Router);
-
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/home',
-      name: 'home',
-      component: TheHome,
-    },
-  ],
-});
-
 new Vue({
   vuetify,
+  router,
   render: (h) => h(App),
 }).$mount('#app');
--- vuetify/vuefy-project2/src/views/TheHome.vue
+++ vuetify/vuefy-project2/src/views/TheHome.vue
@@ -1,5 +1,3 @@
 <template>
-  <div class="home">
-    <h1>Homepage</h1>
-  </div>
+  <h1>Homepage</h1>
 </template>
