6590ec3624569f3d369773b938fd02981a4bddf9
bojkip <bojan.petkovic@protonmail.com>
2022-07-05 13:05:37 +0200

    1.8

--- vuetify/vuefy-project2/src/views/Dashboard.vue
+++ vuetify/vuefy-project2/src/views/Dashboard.vue
@@ -1,26 +1,48 @@
 <template>
   <div class="dashboard">
-    <h1 class="subtitle-1 grey--text">Dashboard</h1>
+    <h1 class="subtitle-1 grey--text pa-4">Dashboard</h1>
 
     <v-container class="my-5">
 
-      <v-card flat pa-3>
-        <v-layout row>
+      <v-layout row class="mb-10">
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn small flat @click="sortBy('title')" v-bind="attrs" v-on="on">
+              <v-icon left small>mdi-folder</v-icon>
+              <span class="caption text-lowercase">By project name</span>
+          </v-btn>
+          </template>
+        <span>Sort by project name</span>
+        </v-tooltip>
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn small flat class="ml-2" @click="sortBy('person')" v-bind="attrs" v-on="on">
+              <v-icon left small>mdi-account</v-icon>
+              <span class="caption text-lowercase">By person</span>
+            </v-btn>
+          </template>
+        <span>Sort by person</span>
+      </v-tooltip>
+      </v-layout>
+
+      <v-card flat class="mt-5" v-for="project in projects" :key="project.title">
+        <v-layout row :class="`pa-4 project ${project.status}`">
           <v-flex xs12 md6>
             <div class="caption grey--text">Project Title</div>
-            <div>Create a new website</div>
+            <div>{{ project.title }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Person</div>
-            <div>Neki lik</div>
+            <div>{{ project.person }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Due By</div>
-            <div>1st Jan 2022</div>
+            <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs2 sm2 md2>
-            <div class="caption grey--text">Status</div>
-            <div>ongoing</div>
+            <div class="right">
+              <v-chip small id="chip" :class="`${project.status} white--text my-2 caption`">{{ project.status }}</v-chip>
+            </div>
           </v-flex>
         </v-layout>
       </v-card>
@@ -28,3 +50,45 @@
     </v-container>
   </div>
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    };
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1);
+    }
+  },
+};
+</script>
+
+<style>
+.project.complete{
+  border-left: 4px solid #3CD1C2;
+}
+.project.ongoing{
+  border-left: 4px solid orange
+}
+.project.overdue{
+  border-left: 4px solid tomato;
+}
+#chip.v-chip.complete{
+  background: #3cd1c2;
+}
+#chip.v-chip.ongoing{
+  background: #ffaa2c;
+}
+#chip.v-chip.overdue{
+  background: #f83e70;
+}
+</style>
