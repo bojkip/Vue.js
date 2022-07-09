d2cf829ca4dca988e9b06f67446fbf3bc28e1f2c
bojkip <bojan.petkovic@protonmail.com>
2022-07-09 22:55:24 +0200

    osnove

---
+++ vuetify/vuefy-project4/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="n in niz" :key="n.id">
+    <p>{{n.text}}</p>
+      <input type="number" v-model="inputNum">
+      <p>{{n.output}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      niz: [
+        {
+          id: 'm', text: 'M To Cm' , input: this.inputNum, output: this.inputNum
+        }
+      ],
+      inputNum: '',
+    }
+  },
+  computed: {
+    outputNumM(num){
+      num * 2
+    }
+  }
+}
+</script>
