84686dd5e6d9d3a7864923f6878e432cd400fef5
Bojki <91488932+bojkip@users.noreply.github.com>
2022-07-26 13:02:53 +0200

    hamburger menu - Nav.vue

---
+++ vuetify/vuefy-project4/src/components/NavSearchBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-card flat width="100em">
+        <v-toolbar class="white--text pt-1" flat>
+            <v-toolbar-title>Search</v-toolbar-title>
+                    <v-autocomplete
+                        v-model="value"
+                        :items="items"
+                        dense
+                        
+                        label="Search"
+                        placeholder="Volume or Temperature"
+                        class="pt-5"
+                    ></v-autocomplete>
+        </v-toolbar>
+        <div class="px-14 pt-4 pb-7">
+                        <v-btn small elevation="0"  rounded @click="searchBtn">
+                            <v-icon left>mdi-magnify</v-icon>
+                            <span>Search</span>
+                        </v-btn>
+                    </div>
+    </v-card>
+</template>
+
+<script>
+    export default {
+    data: () => ({
+        items: [
+            'Energy', 'Length', 'Temperature',
+        ],
+        value: '',
+    }),
+    methods: {
+        searchBtn(){
+            if(this.value === 'Energy'){
+                return this.$router.push('/energy');
+            }
+            else if(this.value === 'Length'){
+                return this.$router.push('/');
+            }
+            else if(this.value === 'Temperature'){
+                return this.$router.push('/temperature');
+            }
+        },
+    }
+    }
+</script>
